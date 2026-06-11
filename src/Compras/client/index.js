80ff7388fdfe85675c59d06d70c49935eace0247
Myltiane Alves <myltiane.aux@gmail.com>
2026-06-11 16:37:48 -0300

    feat: add compras atualizarPedido services

--- src/Compras/client/index.js
+++ src/Compras/client/index.js
@@ -90,6 +90,71 @@ export class ComprasClient {
         return response.data;
     }
 
+    async atualizarPedido(
+        IDRESUMOPEDIDO,
+        IDGRUPOEMPRESARIAL,
+        IDSUBGRUPOEMPRESARIAL,
+        IDCOMPRADOR,
+        IDCONDICAOPAGAMENTO,
+        IDFORNECEDOR,
+        IDTRANSPORTADORA,
+        IDANDAMENTO,
+        MODPEDIDO,
+        NOVENDEDOR,
+        EEMAILVENDEDOR,
+        DTPEDIDO,
+        DTPREVENTREGA,
+        TPFRETE,
+        DESCPERC01,
+        DESCPERC02,
+        DESCPERC03,
+        PERCCOMISSAO,
+        VRTOTALLIQUIDO,
+        OBSPEDIDO,
+        OBSPEDIDO2,
+        DTFECHAMENTOPEDIDO,
+        DTCADASTRO,
+        TPARQUIVO,
+        STDISTRIBUIDO,
+        STAGRUPAPRODUTO,
+        STCANCELADO,
+        TPFISCAL,
+        STRASCUNHO
+    ) {
+        const response = await this.api.put(`${url}/api/compras/atualizar-pedido.xsjs`, [{
+            IDRESUMOPEDIDO,
+            IDGRUPOEMPRESARIAL,
+            IDSUBGRUPOEMPRESARIAL,
+            IDCOMPRADOR,
+            IDCONDICAOPAGAMENTO,
+            IDFORNECEDOR,
+            IDTRANSPORTADORA,
+            IDANDAMENTO,
+            MODPEDIDO,
+            NOVENDEDOR,
+            EEMAILVENDEDOR,
+            DTPEDIDO,
+            DTPREVENTREGA,
+            TPFRETE,
+            DESCPERC01,
+            DESCPERC02,
+            DESCPERC03,
+            PERCCOMISSAO,
+            VRTOTALLIQUIDO,
+            OBSPEDIDO,
+            OBSPEDIDO2,
+            DTFECHAMENTOPEDIDO,
+            DTCADASTRO,
+            TPARQUIVO,
+            STDISTRIBUIDO,
+            STAGRUPAPRODUTO,
+            STCANCELADO,
+            TPFISCAL
+        }]);
+      
+        return response.data;
+    }
+
     async atualizarDistribuicaoHistorico(
         IDDISTRIBUICAOCOMPRASHISTORICO,
         IDPEDIDOCOMPRA,
