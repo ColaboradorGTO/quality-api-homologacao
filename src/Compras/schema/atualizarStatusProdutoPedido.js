4de691334358559631243dfaf0bd5e46079613dc
Myltiane Alves <myltiane.aux@gmail.com>
2026-06-12 10:51:17 -0300

    feat: add compras putAtualizarStatusProdutoPedido services e client schema

---
+++ src/Compras/schema/atualizarStatusProdutoPedido.js
@@ -0,0 +1,30 @@
+import Joi from 'joi';
+
+const atualizarStatusProdutoPedidoSchema = Joi.object({
+    IDDETALHEPEDIDO: Joi.number().integer().required()
+    .messages({
+        'number.base': 'IDDETALHEPEDIDO deve ser um número inteiro',
+        'any.required': 'ID do detalhe do pedido é obrigatório'
+    }),
+    STCANCELADO: Joi.string().allow('').max(10).optional()
+    .messages({
+        'string.base': 'STCANCELADO deve ser uma string',
+        'string.max': 'STCANCELADO deve ter no máximo 10 caracteres'
+    }),
+    IDRESPCANCELAMENTO: Joi.number().integer().required()
+    .messages({
+        'number.base': 'IDRESPCANCELAMENTO deve ser um número inteiro',
+        'any.required': 'ID do responsável pelo cancelamento é obrigatório'
+    }),
+    TXTOBSCANCELAMENTO: Joi.string().allow('').max(500).optional()
+    .messages({
+        'string.base': 'TXTOBSCANCELAMENTO deve ser uma string',
+        'string.max': 'TXTOBSCANCELAMENTO deve ter no máximo 500 caracteres'
+    }),
+    IDRESUMOPEDIDO: Joi.number().integer()
+    .messages({
+        'number.base': 'IDRESUMOPEDIDO deve ser um número inteiro',
+    })
+});
+
+export default atualizarStatusProdutoPedidoSchema;
