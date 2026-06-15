c77f6260291b73dda97f01af51a285a3d6ce8664
LucasEutacio <lucas123@gmail.com>
2026-06-15 15:55:20 -0300

    Feat: Criação de Api de Categoria Receita

--- src/CategoriaReceita/routes/index.js
+++ src/CategoriaReceita/routes/index.js
@@ -1,12 +1,9 @@
 import { Router } from 'express';
 import CategoriaReceitaDespesasControllers from '../controllers/CategoriaReceitaDespesa.js';
 
-
 const categoriaRoutes = new Router();
-
 
 categoriaRoutes.get('/categoria-receita-despesa', CategoriaReceitaDespesasControllers.getListaCategoriaDespesas)
 categoriaRoutes.get('/categoriaReceitaDespesaFinanceira', CategoriaReceitaDespesasControllers.getListaCategoriaDespesasFinanceira)
-
 
 export default categoriaRoutes;
