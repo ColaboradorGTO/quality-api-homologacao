ebaad9203e204feb8b9c51840564a42767c46547
Myltiane Alves <myltiane.aux@gmail.com>
2026-09-01 13:11:10 -0300

    FEAT: ADD PROMOCAO

--- src/Financeiro/Adiantamentos/service/serviceAdiantamento.js
+++ src/Financeiro/Adiantamentos/service/serviceAdiantamento.js
@@ -35,4 +35,134 @@ export class AdiantamentoService {
         )
         return result;
     }
+
+    async updateAdiantamentoDepartamento(
+        IDADIANTAMENTO,
+        DEPARTAMENTO,
+        IDEMPRESA,
+        NUCNPJEMPRESA,
+        POSSUINOTAFISCAL,
+        CNPJFATURAMENTO,
+        VRSOLICITADO,
+        DESCRICAO,
+        ANEXOORCAMENTO,
+        ANEXONOTAFISCAL,
+        RAZAOSOCIALFATURAMENTO,
+        STATUS,
+        DSJUSTIFICATIVA,
+        IDUSUARIOALTERACAO
+    ) {
+        if (!IDEMPRESA) {
+            throw new Error("IDEMPRESA is required, services");
+        }
+
+        const result = await this.client.atualizarAdiantamentoDepartamento(
+            IDADIANTAMENTO,
+            DEPARTAMENTO,
+            IDEMPRESA,
+            NUCNPJEMPRESA,
+            POSSUINOTAFISCAL,
+            CNPJFATURAMENTO,
+            VRSOLICITADO,
+            DESCRICAO,
+            ANEXOORCAMENTO,
+            ANEXONOTAFISCAL,
+            RAZAOSOCIALFATURAMENTO,
+            STATUS,
+            DSJUSTIFICATIVA,
+            IDUSUARIOALTERACAO
+        )
+        return result;
+    }
+
+    async updatePagamentoDepartamento(
+        IDPAGAMENTO,
+        IDADIANTAMENTO,
+        VLPAGAMENTO,
+        DATAPAGAMENTO,
+        FORMAPAGAMENTO,
+        ANEXOCOMPROVANTE,
+        DSOBSERVACAO,
+        STATUS,
+        IDUSUARIOCRIACAO
+    ) {
+        if (!IDPAGAMENTO) {
+            throw new Error("IDPAGAMENTO is required, services");
+        }
+
+        const result = await this.client.atualizarPagamentoDepartamento(
+            IDPAGAMENTO,
+            IDADIANTAMENTO,
+            VLPAGAMENTO,
+            DATAPAGAMENTO,
+            FORMAPAGAMENTO,
+            ANEXOCOMPROVANTE,
+            DSOBSERVACAO,
+            STATUS,
+            IDUSUARIOCRIACAO
+        )
+        return result;
+    }
+
+    async createAdiantamentoDepartamento(
+        DEPARTAMENTO,
+        IDEMPRESA,
+        NUCNPJEMPRESA,
+        POSSUINOTAFISCAL,
+        CNPJFATURAMENTO,
+        VRSOLICITADO,
+        DESCRICAO,
+        ANEXOORCAMENTO,
+        ANEXONOTAFISCAL,
+        RAZAOSOCIALFATURAMENTO,
+        DSJUSTIFICATIVA,
+        IDUSUARIOCRIACAO
+    ) {
+        if (!IDEMPRESA) {
+            throw new Error("IDEMPRESA is required, services");
+        }
+
+        const result = await this.client.criarAdiantamentoDepartamento(
+            DEPARTAMENTO,
+            IDEMPRESA,
+            NUCNPJEMPRESA,
+            POSSUINOTAFISCAL,
+            CNPJFATURAMENTO,
+            VRSOLICITADO,
+            DESCRICAO,
+            ANEXOORCAMENTO,
+            ANEXONOTAFISCAL,
+            RAZAOSOCIALFATURAMENTO,
+            DSJUSTIFICATIVA,
+            IDUSUARIOCRIACAO
+        )
+        return result;
+    }
+
+    async createPagamentoDepartamento(
+        IDADIANTAMENTO,
+        VLPAGAMENTO,
+        DATAPAGAMENTO,
+        FORMAPAGAMENTO,
+        ANEXOCOMPROVANTE,
+        DSOBSERVACAO,
+        STATUS,
+        IDUSUARIOCRIACAO
+    ) {
+        if (!IDADIANTAMENTO) {
+            throw new Error("IDADIANTAMENTO is required, services");
+        }
+
+        const result = await this.client.criarPagamentoDepartamento(
+            IDADIANTAMENTO,
+            VLPAGAMENTO,
+            DATAPAGAMENTO,
+            FORMAPAGAMENTO,
+            ANEXOCOMPROVANTE,
+            DSOBSERVACAO,
+            STATUS,
+            IDUSUARIOCRIACAO
+        )
+        return result;
+    }
 }
--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,5 +1,7 @@
 const corsMiddleware = function(req, res, next)  {
-    res.header('Content-Type', 'application/json; charset=utf-8');
+    if (!req.path.startsWith('/files')) {
+        res.header('Content-Type', 'application/json; charset=utf-8');
+    }
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Device, user.login, User-Agent, user.password, skiploading, skiperror, token");
     res.header("Access-Control-Allow-Methods", "POST,PUT,DELETE,GET,OPTIONS");
